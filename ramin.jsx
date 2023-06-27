9e80a8d96bc68dfbeaecac9d45348d1523a9ae20
Ramin Karimpor <ramin.kp81@gmail.com>
2023-06-27 17:50:17 +0330

    new projectttttttttttt

---
+++ ramin.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ramin extends Component {
+    render() {
+        return (
+            <div>
+                raminnnnnnnnnnnnnnnnnnnnnnnnnnnn
+            </div>
+        );
+    }
+}
+
+export default ramin;
