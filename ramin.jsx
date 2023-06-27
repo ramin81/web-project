3918013313ff9979291a12b99e29b96da8af9119
Ramin Karimpor <ramin.kp81@gmail.com>
2023-06-27 17:56:10 +0330

    delete n

---
+++ ramin.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ramin extends Component {
+    render() {
+        return (
+            <div>
+                ramin
+            </div>
+        );
+    }
+}
+
+export default ramin;
